79daf15c44e9b6c02954a40778c1a06960b40945
shrria <fctmaneepakorn5@gmail.com>
2022-01-22 22:11:41 +0700

    ADD HttpStatusCode.

--- api/views.py
+++ api/views.py
@@ -1,34 +1,40 @@
+import string
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
 
 
 def home(request):
     return render(request, 'home.html')
 
 def is_match(request):
-    if request.method == 'GET':
-        message = request.GET['message']
-        pattern = request.GET['pattern']
+    try:
+        if request.method == 'GET':
+            message = request.GET['message']
+            pattern = request.GET['pattern']
 
-        def match_check(msg, ptn):
-            if (len(msg)==0 and len(ptn)==0):
-                return True
-            if ((len(msg)==0 and len(ptn)>0) or (len(msg)>0 and len(ptn)==0)):
+            def match_check(msg:string, ptn:string):
+                if (len(msg)==0 and len(ptn)==0):
+                    return True
+                if ((len(msg)==0 and len(ptn)>0) or (len(msg)>0 and len(ptn)==0)):
+                    return False
+                elif (msg[0] == ptn[0] or ptn[0] == '?'):
+                    return match_check(msg[1:] ,ptn[1:])
+                elif (ptn[0] == '*'):
+                    return True
                 return False
-            elif (msg[0] == ptn[0] or ptn[0] == '?'):
-                return match_check(msg[1:] ,ptn[1:])
-            elif (ptn[0] == '*'):
-                return True
-            return False
 
-        status = match_check(message, pattern)
-        
-        response = {
-            'is_match' : status
+            body = {
+                'is_match' : match_check(message, pattern)
+            }
+
+            return HttpResponse(json.dumps(body), status=200)
+    
+    except:
+
+        body = {
+                'is_match' : None
         }
-
-        return JsonResponse(response)
-
-    # return HttpResponse('<h1>You give me something?</h1>')
+        
+        return HttpResponse(json.dumps(body), status=400)
 
